b23f8e9c6a862855718cb6e7906c7afdce6095fb
muhammet <98078090+muhammet72@users.noreply.github.com>
2023-06-02 21:18:03 +0300

    .x

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,13 @@ import React from "react"
 
 const Header = () => {
   return (
-    <div>
-      Decentralized Lottery
-      <ConnectButton moralisAuth={false} />
-    </div>
-  )
+    <nav className="p-5 border-b-2 flex flex-row">
+        <h1 className="py-4 px-4 font-bold text-3xl"> Decentralized Lottery</h1>
+        <div className="ml-auto py-2 px-4">
+            <ConnectButton moralisAuth={false}/>
+        </div>
+    </nav>
+)
 }
 
 export default Header
